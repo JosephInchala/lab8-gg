ba67a0b064002315a324cc7bceea52e6d0864585
JosephInchala <forthkung5za@gmail.com>
2022-12-20 15:13:06 +0700

    lab completed

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -16,3 +16,17 @@ char before(char x){
         return '0' ;
     }
 }
+
+
+int main(){
+	//Test Case
+	cout << before('A') << "\n";
+	cout << before('B') << "\n";
+	cout << before('P') << "\n";
+	cout << before('T') << "\n";
+	cout << before('Z') << "\n";
+	cout << before('a') << "\n";
+	cout << before('0') << "\n";
+	cout << before('c') << "\n";
+	return 0;
+}
